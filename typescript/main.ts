5ac7fff89e43b8451b1fc83bbdf3f4a080138ee8
dav ost <private>
2023-10-03 23:29:45 +0300

    added a main.ts file to run

---
+++ typescript/main.ts
@@ -0,0 +1,32 @@
+import userConfig from '../user_config.json';
+import logger, {loggingLevel} from './common/logger';
+import { YouTubePlaylist } from './youtube/playlist';
+import { SpotifyClient } from './spotify/client';
+
+
+const loggingConfig = userConfig.logging;
+const youtubeConfig = userConfig.youtube;
+const spotifyConfig = userConfig.spotify;
+
+async function main() {
+    logger.setLevel(loggingLevel[loggingConfig.level]);
+    
+    const youtubePlaylist = new YouTubePlaylist(youtubeConfig.api_key, youtubeConfig.playlist_id);
+    const spotifyClient = await new SpotifyClient(spotifyConfig.access_token).setUserId();
+
+    // In order to add to an existing playlist, set the playlist id variable instead of calling 'create_playlist' method
+    // const playlistId = "6QI3jq35kjUN0lAWbRQ7o8";
+
+    const playlistId = await spotifyClient.createPlaylist(
+        spotifyConfig.playlist.name, spotifyConfig.playlist.description, spotifyConfig.playlist.public
+    );
+
+    for await (let titlesBatch of youtubePlaylist.titlesBatchGenerator(50)) {
+        const snapshotId = await spotifyClient.addTracks(playlistId, titlesBatch);
+        if (snapshotId) {
+            logger.debug(`Spotify snapshot id of the new added tracks is ${snapshotId}`);
+        }
+    }
+}
+
+main();
